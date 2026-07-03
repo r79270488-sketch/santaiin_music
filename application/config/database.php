e4e251df61356e1099ba5b08d96d6afd7c759888
r79270488-sketch <r79270488@gmail.com>
2026-07-03 16:40:05 +0700

    feat: update database configuration with new credentials

--- application/config/database.php
+++ application/config/database.php
@@ -76,9 +76,9 @@ $query_builder = TRUE;
 $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
-	'username' => '',
-	'password' => '',
-	'database' => '',
+	'username' => 'santaiin_xyz',
+	'password' => '_j@8L5k0Z>(=bj+I',
+	'database' => 'santaiin_xyz',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
